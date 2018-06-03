f9d49f51704ccd3f86a19ab808e7fe714910fda7
Abbe98 <albin.post@gmail.com>
2018-06-03 17:32:11 +0200

    linting change

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'semi': [2, 'always'],
     'comma-dangle': [2, 'always-multiline'],
+    'space-before-function-paren': [2, 'never'],
   },
   parserOptions: {
     parser: 'babel-eslint',
