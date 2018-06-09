f1c17ebd247313dd44b60970452f6860eb1a0508
Abbe98 <albin.post@gmail.com>
2018-06-09 16:10:59 +0200

    refactor for language item toggels

---
+++ src/components/Item.vue
@@ -0,0 +1,48 @@
+<template>
+  <li>
+    <span role="link" v-bind:aria-expanded="open" v-on:click="toggle()">
+      {{ item.itemLabel.value }} <small v-if="item.itemDescription">{{ item.itemDescription.value }}</small>
+    </span>
+    <section v-bind:class="{ active: open }">stuff</section>
+  </li>
+</template>
+
+<script>
+
+export default {
+  name: 'Item',
+  data: function() {
+    return {
+      open: false,
+    }
+  },
+  props: {
+    item: Object,
+  },
+  /*asyncComputed: {
+    image: {
+      get() {
+        return this.$http
+          .get(`sparql?query=${RQLanguageItems(this.$route.params.id)}`)
+          .then(response => response.body);
+      },
+      default: false,
+    },
+  },*/
+  methods: {
+    toggle: function() {
+      this.open = !this.open;
+    }
+  },
+};
+</script>
+
+<style scoped>
+section {
+  display: none;
+}
+
+section.active {
+  display: block;
+}
+</style>
--- src/views/Language.vue
+++ src/views/Language.vue
@@ -1,25 +1,22 @@
 <template>
-  <div class="home">
+  <div>
     <Spinner v-if="!items" size="huge" message="Loading..."></Spinner>
     <ul v-else>
-      <li v-for="item in items.results.bindings" v-bind:key="item.itemQ.value" tag="li">
-        <span role="link" aria-expanded="true">
-          {{ item.itemLabel.value }} <small v-if="item.itemDescription">{{ item.itemDescription.value }}</small>
-        </span>
-        <section>stuff</section>
-      </li>
+      <Item v-for="item in items.results.bindings" v-bind:item="item" v-bind:key="item.itemQ.value"/>
     </ul>
   </div>
 </template>
 
 <script>
 import Spinner from 'vue-simple-spinner';
+import Item from '../components/Item';
 import { RQLanguageItems } from '../queries';
 
 export default {
-  name: 'Home',
+  name: 'Language',
   components: {
     Spinner,
+    Item,
   },
   asyncComputed: {
     items: {
