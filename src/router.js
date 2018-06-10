d88cd21d19ee3c143a793d7aaa1acbe881027e53
Abbe98 <albin.post@gmail.com>
2018-06-10 20:18:30 +0200

    flashcards view structure

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Language from './views/Language.vue';
+import Home from './views/Home';
+import Language from './views/Language';
+import Cards from './views/Cards';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/lang/:id',
       name: 'lang',
       component: Language,
+    },
+    {
+      path: '/lang/:id/cards',
+      name: 'cards',
+      component: Cards,
     },
   ],
 });
---
+++ src/views/Cards.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Flashcards<span>{{language}} flashcards</span></h1>
+  </div>
+</template>
+
+<script>
+import Spinner from 'vue-simple-spinner';
+import Footer from '../components/Footer';
+
+export default {
+  name: 'Cards',
+  components: {
+    Spinner,
+    Footer,
+  },
+  data: function() {
+    return {
+      language: false,
+    };
+  },
+  created: function() {
+    this.$http.wikidata
+      .get(`w/api.php?action=wbsearchentities&search=${this.$route.params.id}&language=en&origin=*&format=json`)
+      .then(response => {
+        this.language = response.data.search[0].label;
+        document.title = this.language + ' Flashcards';
+      });
+  },
+};
+</script>
