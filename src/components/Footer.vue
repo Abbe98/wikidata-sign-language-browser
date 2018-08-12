68c0b193663ba0ff9df4582be302e57139c74443
Abbe98 <albin.post@gmail.com>
2018-08-12 19:43:10 +0200

    github and contact in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,7 @@
       <a href="https://www.wikidata.org/wiki/Wikidata:Main_Page">
         <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/Wikidata_Stamp_Rec_Light.svg" alt="This application is powered by Wikidata." />
       </a>
+      <p>User: <a href="https://meta.wikimedia.org/wiki/User:Abbe98">Abbe98</a> • <a href="https://github.com/Abbe98/wikidata-sign-language-browser">Source code</a></p>
   </footer>
 </template>
 
@@ -16,6 +17,10 @@ export default {
 footer {
     text-align: center;
     margin-top: 1.8rem;
+}
+
+a {
+   color: #fff;
 }
 
 img {
