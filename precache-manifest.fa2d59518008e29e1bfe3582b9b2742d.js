451003510700c0260a9e8ae93018e50cb76c36a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-12 17:33:53 +0000

    Deploy Abbe98/wikidata-sign-language-browser to github.com/Abbe98/wikidata-sign-language-browser.git:build

---
+++ precache-manifest.fa2d59518008e29e1bfe3582b9b2742d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "37a67a0e1d7a75bb14cf",
+    "url": "/js/chunk-vendors.37a67a0e.js"
+  },
+  {
+    "revision": "9f26499f56df54767544",
+    "url": "/js/app.9f26499f.js"
+  },
+  {
+    "revision": "f377edb0e43fc4a65365d5680964780e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f26499f56df54767544",
+    "url": "/css/app.ed76f87d.css"
+  }
+];
