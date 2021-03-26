9d489fd139f0343de6dd169dc22d24bc72fc068d
Abbe98 <albin.post@gmail.com>
2021-03-26 21:02:40 +0100

    check the language of the statement #2

--- src/queries.js
+++ src/queries.js
@@ -1,8 +1,9 @@
 export const RQ_LANGUAGES = encodeURI(`SELECT (COUNT(?lang) as ?count) ?langLabel (REPLACE(STR(?lang), "http://www.wikidata.org/entity/", "") as ?langQ)
 WHERE  {
-  ?item wdt:P2919 ?file .
   ?item p:P2919 ?statement .
-  ?statement pq:P407 ?lang .
+  ?statement ps:P2919 ?file .
+  ?statement pq:P407 ?lang .  
+
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" . }
 }
 GROUP BY ?langLabel ?lang
@@ -15,8 +16,8 @@ export function RQLanguageItems(language) {
   ?itemDescription
 
   WHERE {
-    ?item wdt:P2919 ?file .
     ?item p:P2919 ?statement .
+    ?statement ps:P2919 ?file .
     ?statement pq:P407 wd:${language} .
     SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" . }
   }
